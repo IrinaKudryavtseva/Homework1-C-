31b6a9dd7d09cd37d868a6854cce5cddf18c6be7
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-15 14:13:30 +0500

    Homework Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+   Console.Write("Max = ");
+   Console.Write(number1);
+}
+else if(number2 > number1)
+{
+   Console.Write("Max = ");
+   Console.Write(number2);
+}
+else
+{
+   Console.WriteLine("Числа равны между собой");
+}
